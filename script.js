fa33fbb5f5a620cbed8a72cddc8d8f9e202fda1c
nonobromo <noamab22@gmail.com>
2024-03-10 01:05:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,101 @@
+const answers = [
+    [1, 1], [2, 2], [4, 4], [3, 2], [6, 12], [5, 7], [18, 12], [17, 17], [24, 16], [7, 8], [12, 7], [11, 13], [15, 12], [24, 32], [25, 15], [35, 45], [65, 35], [19, 19], [16, 16], [15, 15][24, 24], [27, 35], [9, 5], [18, 19], [31, 45], [3, 3], [2, 3]
+];
+
+const opts = ["+", "-"]
+
+const question = document.querySelector("#show-qst");
+
+let score = 0;
+let totalscore = 0;
+
+let num1 = 0;
+let num2 = 0;
+
+let isCorrect = false;
+let showCorrect;
+
+let signOp;
+
+function myFunc() {
+    const j = Math.floor(Math.random() * answers.length);
+    const k = Math.floor(Math.random() * opts.length)
+    for (let i = 0; i < 1; i++) {
+        num1 = answers[j][0];
+        num2 = answers[j][1];
+        signOp = opts[k]
+        result = eval(`${num1} ${signOp} ${num2}`);
+        question.innerHTML = num1 + opts[k] + num2 + " = ";
+    }
+}
+
+myFunc()
+
+
+const btnCheck = document.getElementById("btn");
+
+btnCheck.addEventListener("click", function () {
+    let input = document.getElementById("submit").value;
+    let btnCheck = document.getElementById("btn");
+
+    if (+input === result) {
+        btnCheck.innerHTML = "Correct!";
+        btnCheck.style.backgroundColor = "#2BD400";
+        isCorrect = true;
+        showCorrect = setTimeout(statusUpdate, 1500);
+        score = 10;
+        totalscore += 10;
+    } else if (+input != result || +input === "") {
+        btnCheck.innerHTML = "Incorrect!"
+        btnCheck.style.backgroundColor = "#F28F1A";
+        isCorrect = false;
+        showCorrect = setTimeout(statusUpdate, 1500);
+        score = 0;
+    }
+    console.log(totalscore);
+    generateResult(input);
+    myFunc();
+    document.getElementById("submit").value = "";
+    document.getElementById("score-show").innerHTML = "Your Score is " + totalscore;
+});
+
+
+function generateResult() {
+    let input = document.getElementById("submit").value;
+    const resultAdd = document.querySelector("#add-result");
+    const divAdd = document.createElement("div");
+    divAdd.classList.add("result-add");
+    divAdd.style.textAlign = "left";
+    resultAdd.appendChild(divAdd);
+
+    const probelm = document.createElement("h3");
+    const correct = document.createElement("h3");
+    const givenAnswer = document.createElement("h3");
+    const scoreGiven = document.createElement("h3");
+
+    probelm.innerHTML = num1 + signOp + num2;
+    correct.innerHTML = result;
+    givenAnswer.innerHTML = input;
+    scoreGiven.innerHTML = score;
+
+    if (isCorrect === true) {
+        givenAnswer.style.color = "#2BD400";
+    } else {
+        givenAnswer.style.color = "#F28F1A"
+    }
+
+    if (input === "") {
+        givenAnswer.innerHTML = "Empty";
+    }
+
+    divAdd.appendChild(probelm);
+    divAdd.appendChild(correct);
+    divAdd.appendChild(givenAnswer);
+    divAdd.appendChild(scoreGiven)
+}
+
+function statusUpdate() {
+    btnCheck.innerHTML = "Check";
+    btnCheck.style.backgroundColor = "#0019FF";
+    btnCheck.style.transition = "400ms";
+}
